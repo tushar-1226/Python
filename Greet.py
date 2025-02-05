a16405f4c97a452d9a15c0a4e777547b39ed9fbc
tushar-1226 <rockeytushar17@gmail.com>
2025-02-05 23:00:21 +0530

    Update Greet.py

--- Greet.py
+++ Greet.py
@@ -1,26 +1,3 @@
-# import time
-# name=input("Please enter your name: ")
-# currentTime = int(time.strftime('%H')) 
-
-# if currentTime < 12:
-#   print('Good morning ',name)
-# elif currentTime >12:
-#   print('Good afternoon ',name)
-# elif currentTime > 6:
-#   print('Good night',name)  
-
-
-# import time
-# t = time.strftime('%H:%M:%S')
-# hour = int(time.strftime('%H'))
-
-# if(hour>=0 and hour<12):
-#    print("Good morning!")
-# elif(hour>=12 and hour<17):
-#    print("Good afternoon!")  
-# elif(hour>=17 and hour<0):
-#    print("Good night!")  
-
 import time
 name= input("Enter your name:-").title()
 hour = int(time.strftime('%H'))
